bea1e0c74de5fab9c044220e7a7d7bd52b4546d7
ShenoyRishabh <136888631+ShenoyRishabh@users.noreply.github.com>
2025-06-28 17:35:07 +0530

    UPD Js file

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var PLAYname = prompt("What is your name?");
-alert("welcome, " + PLAYname)
+function guessingame() {
+var num = Math.round(
+  Math.random() * 100 + 1
+)
+var guess = "";
+while (guess != num){
+  guess = prompt("Guess a number between 1 to 100")
+ if (guess > num)
+ alert("Your Guess is too high")
+ else if(guess < num)
+ alert("Your Guess is too low")
+ else if(guess == num)
+ alert("You Win!")
+}
+}
